4802539a72c76a9e6a96b80d83fa3a1c5fa33bcf
razirizwan <razirizwan0908@gmail.com>
2026-01-13 10:49:25 +0530

    initial dokploy test app

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3100;
+
+app.get("/", (req, res) => {
+  res.send("✅ Dokploy GitHub deploy works!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
